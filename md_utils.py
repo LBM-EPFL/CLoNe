5ca2f003cfd19b61da0aef1a6e57112a8533a138
SylvainTraeger <sylvain.traeger@epfl.ch>
2020-06-13 14:05:40 +0200

    Update and rename md_handler.py to md_utils.py
    fixed an issue with parameter loading

--- md_utils.py
+++ md_utils.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 import numpy as np
 import warnings
@@ -5,67 +6,57 @@ from configparser import ConfigParser
 
 
 def load_md_args(parser):
-    parser.add_argument("-c", type=str, default="DEFAULT", help="Loads parameters and filenames from md_config.ini if applicable")
-
     # CLoNe arguments
-    parser.add_argument("-pdc", type=float, default=0, help="Relates to the sigma of Gaussian kernel used to compute local densities of each point. Use integers or half-integer values. Default: 4.")
-    parser.add_argument("-n_resize", type=float, default=0, help="Neighbour matrix resize factor. Reduces the number of neighbours to consider when building the neighbour matrix by dividing the number of elements by the provided value. Defaults to 1 (i.e. all neighbours considered).")
-    parser.add_argument("-filt", type=float, default=0, help="If enabled, filters out outliers.")
+    parser.add_argument("-pdc", type=float, default=4, help="Relates to the sigma of Gaussian kernel used to compute local densities of each point. Use integers or half-integer values. Default: 4.")
+    parser.add_argument("-n_resize", type=float, default=4, help="Neighbour matrix resize factor. Reduces the number of neighbours to consider when building the neighbour matrix by dividing the number of elements by the provided value. Defaults to 1 (i.e. all neighbours considered).")
+    parser.add_argument("-filt", type=float, default=0.1, help="If enabled, filters out outliers.")
     parser.add_argument("-verbose", action='store_true', help="CLoNe verbosity")          
 
     # MD-specific arguments
+    parser.add_argument("-c", type=str, default="DEFAULT", help="Loads parameters and filenames from md_config.ini if applicable")
     parser.add_argument("-traj", type=str, default="", help="Path and filename of trajectory to cluster")
     parser.add_argument("-topo", type=str, default="", help="Path and filename of topology file. Leave blank in case of multiframe PDB")
     parser.add_argument("-at_sel", type=str, default="name CA or name BB", help="Atom selection. Default to 'name CA or name BB'. Follows MDAnalysis' naming conventions. Resulting features used for clustering. If PCA is enabled, it will be applied on the selection.")
     parser.add_argument("-feat", type=str, default="", help="Text file containing features to use for clustering. If PCA is enabled, it will be applied on these features.")
     parser.add_argument("-pca", type=int, default=0, help="Whether to perform PCA on vectors. Value corresponds to the number of principal component to consider.")
     args = parser.parse_args()
-
-    # Check if parameter sets already exists in md_confid.ini; else, load default.
     c_section = args.c
-    config = ConfigParser()
-    config.read('md_config.ini')
-    if c_section not in config.sections():
-        assert args.traj, "Invalid input arguments and no trajectory specified"
-        print("> Loading default parameters")
-        c_section = "DEFAULT"
-    else:
-        print("> Loading config for %s"%c_section)
-    c = config[c_section]
-
-    # Get values from md_config.ini
-    pdc = c.getfloat('pdc')
-    n_resize = c.getfloat('n_resize')
-    filt = c.getfloat('filt')
-    verbose = c.getboolean('verbose')
-    traj = c.get('traj')
-    topo = c.get('topo')
-    at_sel = c.get('at_sel')
-    feat = c.get('feat')
-    pca = c.getint('pca')
-
-    # Update from command-line arguments
-    # CLoNe
-    if args.pdc:
+    
+    if c_section == "DEFAULT":
+        # CLoNe
         pdc = args.pdc
-    if args.n_resize:
         n_resize = args.n_resize
-    if args.filt:
         filt = args.filt
-    if args.verbose:
         verbose = args.verbose
 
-    # MD-specific
-    if args.traj:
+        # MD-specific
         traj = args.traj
-    if args.topo:
         topo = args.topo
-    if args.at_sel:
         at_sel = args.at_sel
-    if args.feat:
         feat = args.feat
-    if args.pca:
         pca = args.pca
+
+        system_name = os.path.splitext(os.path.split(traj)[1])[0]
+
+    else:
+        # Check if parameter sets already exists in md_confid.ini; else, load default.
+        config = ConfigParser()
+        config.read('md_config.ini')
+        assert c_section in config.sections(), "Invalid config name; check md_config.ini for valid names"
+        print("> Loading config for %s"%c_section)
+        c = config[c_section]
+
+        # Get values from md_config.ini
+        pdc = c.getfloat('pdc')
+        n_resize = c.getfloat('n_resize')
+        filt = c.getfloat('filt')
+        verbose = c.getboolean('verbose')
+        traj = c.get('traj')
+        topo = c.get('topo')
+        at_sel = c.get('at_sel')
+        feat = c.get('feat')
+        pca = c.getint('pca')
+
     return [pdc, n_resize, filt, verbose, traj, topo, at_sel, feat, pca, c_section]
 
 def show_cluster_info(clone, data, path, headers):
